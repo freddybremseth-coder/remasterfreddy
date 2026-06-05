a79e04a9edbfae21a6ab09ffb9900914d5a7e019
Freddy Bremseth <freddy.bremseth@gmail.com>
2026-06-05 13:22:34 +0200

    Show admin entry on public site

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AdminApp from "./AdminApp";
+import AdminEntry from "./AdminEntry";
 import "./styles.css";
 
-const page = window.location.pathname === "/admin" ? <AdminApp /> : <App />;
+function PublicSite() {
+  return (
+    <>
+      <App />
+      <AdminEntry />
+    </>
+  );
+}
+
+const page = window.location.pathname === "/admin" ? <AdminApp /> : <PublicSite />;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>{page}</React.StrictMode>,
